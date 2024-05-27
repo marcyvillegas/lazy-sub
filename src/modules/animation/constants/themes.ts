51916e07c172f2962a9c36913505212164f60439
Marcy Villegas <marcyvillegas17@gmail.com>
2024-05-27 14:18:58 +0800

    Added color themes

--- src/modules/animation/constants/themes.ts
+++ src/modules/animation/constants/themes.ts
@@ -5,8 +5,18 @@ export const themes = [
     text: 'text-main-background justify-center',
   },
   {
+    name: 'Light Rounded',
+    style: 'bg-white-tab rounded-full p-2 px-4',
+    text: 'text-main-background justify-center',
+  },
+  {
     name: 'Dark',
     style: 'bg-main-background p-2',
+    text: 'text-white-tab justify-center',
+  },
+  {
+    name: 'Dark Rounded',
+    style: 'bg-main-background rounded-full p-2 px-4',
     text: 'text-white-tab justify-center',
   },
 ];
