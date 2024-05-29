447e3b2aedff264f8957609a5bb598f9c4d569ac
Marcy Villegas <marcyvillegas17@gmail.com>
2024-05-29 11:58:55 +0800

    Update example page

--- src/modules/examples/index.tsx
+++ src/modules/examples/index.tsx
@@ -2,7 +2,8 @@ export default function Examples() {
     return (
         <div className="p-7">
             <div className="flex justify-center text-center text-main-white font-semibold">
-                Check out some examples of text animations from LazySub 👀
+                <div>Check out some examples of text animations from LazySub 👀</div>
+                <div>You can also submit your videos here to be featured in this page!</div>
             </div>
 
             <div className="h-full lg:h-screen lg:w-full p-6 flex flex-col lg:flex-row justify-center mb-10 gap-8 lg:gap-[80px]">
@@ -17,7 +18,7 @@ export default function Examples() {
                     className="h-screen"
                     src="https://www.tiktok.com/embed/7374000217386994950"
                     scrolling="no"
-                    llow="encrypted-media;"
+                    allow="encrypted-media;"
                 ></iframe>
             </div>
         </div>
