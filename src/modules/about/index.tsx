ecde0aab6224142a8ca1b46a1faa4d9a01c59bcd
Marcy Villegas <marcyvillegas17@gmail.com>
2024-05-30 11:14:13 +0800

    Added target blank on links

--- src/modules/about/index.tsx
+++ src/modules/about/index.tsx
@@ -17,7 +17,7 @@ export default function About() {
 
             <div>Why I created LazySub?</div>
             <div>
-                This project is inspired by {`devaslife's`} {`(Takuya Matsuyama)`} <Link href={'https://www.youtube.com/watch?v=zu_vqAWHy_E&t=2s'}><span className="underline decoration-logo-green decoration-2">video editing style</span></Link> wherein he specifically uses a chat bubble text animation. He deserves the recognition so check out his two youtube channels: <Link href={'https://www.youtube.com/@devaslife'}><span className="underline decoration-logo-green decoration-2">devaslife</span></Link> and <Link href={'https://www.youtube.com/@craftzdog'}><span className="underline decoration-logo-green decoration-2">Takuya Matsuyama</span></Link>
+                This project is inspired by {`devaslife's`} {`(Takuya Matsuyama)`} <Link href={'https://www.youtube.com/watch?v=zu_vqAWHy_E&t=2s'} target="_blank"><span className="underline decoration-logo-green decoration-2">video editing style</span></Link> wherein he specifically uses a chat bubble text animation. He deserves the recognition so check out his two youtube channels: <Link href={'https://www.youtube.com/@devaslife'} target="_blank"><span className="underline decoration-logo-green decoration-2">devaslife</span></Link> and <Link href={'https://www.youtube.com/@craftzdog'}><span className="underline decoration-logo-green decoration-2">Takuya Matsuyama</span></Link>
             </div>
 
             <div>
@@ -25,11 +25,11 @@ export default function About() {
             </div>
 
             <div>
-                I plan to add more features in the future.  <Link href={'https://forms.gle/KkUyWqFiQkakFpPt8'}><span className="underline decoration-logo-green decoration-2">Feel free to give suggestions, feedback, feature request or bug report!</span></Link> You can also check the source code <Link href={'https://github.com/marcyvillegas/lazy-sub'}><span className="underline decoration-logo-green decoration-2">here</span></Link>
+                I plan to add more features in the future.  <Link href={'https://forms.gle/KkUyWqFiQkakFpPt8'} target="_blank"><span className="underline decoration-logo-green decoration-2">Feel free to give suggestions, feedback, feature request or bug report!</span></Link> You can also check the source code <Link href={'https://github.com/marcyvillegas/lazy-sub'} target="_blank"><span className="underline decoration-logo-green decoration-2">here</span></Link>
             </div>
 
             <div>
-                Coded by <Link href={'https://marcyvillegas.netlify.app/'}><span className="underline decoration-logo-green decoration-2">Marcy Villegas</span></Link>
+                Coded by <Link href={'https://marcyvillegas.netlify.app/'} target="_blank"><span className="underline decoration-logo-green decoration-2">Marcy Villegas</span></Link>
             </div>
         </div >
     )
--- src/modules/examples/index.tsx
+++ src/modules/examples/index.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Examples() {
     return (
         <div className="p-7">
             <div className="text-center text-main-white font-semibold">
                 <div>Check out some examples of text animations from LazySub 👀</div>
-                <div>You can also submit your videos here to be featured in this page!</div>
+                <div>You can also submit your videos <Link href={'https://forms.gle/KkUyWqFiQkakFpPt8'} target="_blank"><span className="underline decoration-logo-green decoration-2">here</span></Link> to be featured in this page!</div>
             </div>
 
             <div className="h-full lg:h-screen lg:w-full p-6 flex flex-col lg:flex-row justify-center mb-10 gap-8 lg:gap-[80px]">
